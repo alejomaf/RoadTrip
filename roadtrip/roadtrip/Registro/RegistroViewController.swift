bd500cadae19a76c420993fa3f2900413c3870d3
Alejo Martín Arias Filippo <alejomaf@outlook.com>
2021-01-18 18:31:37 +0100

    Controllers updated

--- roadtrip/roadtrip/Registro/RegistroViewController.swift
+++ roadtrip/roadtrip/Registro/RegistroViewController.swift
@@ -11,19 +11,51 @@ import UIKit
 class RegistroViewController: UIViewController {
 
     
-    @IBOutlet weak var botonCancelar: UIBarButtonItem!
-    @IBOutlet weak var botonGuardar: UIBarButtonItem!
-    @IBOutlet weak var barraNavegacion: UINavigationItem!
-    @IBOutlet weak var texto: UILabel!
+    @IBOutlet weak var textoA1: UITextField!
+    @IBOutlet weak var textoA2: UITextField!
+    @IBOutlet weak var textoA3: UITextField!
+    @IBOutlet weak var textoA4: UITextField!
+    @IBOutlet weak var textoA5: UITextField!
+    @IBOutlet weak var textoA6: UITextField!
+    
+    
+    @IBOutlet weak var sliderA1: UISlider!
+    @IBOutlet weak var sliderA2: UISlider!
+    @IBOutlet weak var sliderA3: UISlider!
+    @IBOutlet weak var sliderA4: UISlider!
+    @IBOutlet weak var sliderA5: UISlider!
+    @IBOutlet weak var sliderA6: UISlider!
+    
+    
     @IBOutlet weak var botonNo: UIButton!
     @IBOutlet weak var botonSi: UIButton!
-    @IBOutlet weak var listaRegistros: UIStackView!
-    @IBOutlet weak var botones: UIStackView!
+    
+    @IBOutlet weak var mapa: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func editarTexto(_ sender: Any) {
+        
+    }
+    @IBAction func editarTextoA2(_ sender: Any) {
+    }
+    @IBAction func editarTextoA3(_ sender: Any) {
+    }
+    @IBAction func editarTextoA4(_ sender: Any) {
+    }
+    @IBAction func editarTextoA5(_ sender: Any) {
+    }
+    @IBAction func editarTextoA6(_ sender: Any) {
+    }
+    
+    
+    @IBAction func pulsarBotonSi(_ sender: Any) {
+    }
+    @IBAction func pulsaBotonNo(_ sender: Any) {
     }
     
 
